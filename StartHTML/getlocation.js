3a3c86bb9f1575394c59d978e7afb25520d23de4
Miaoqi Wang <miaoqi0119@163.com>
2017-05-08 21:49:58 +0800

    get method

--- StartHTML/getlocation.js
+++ StartHTML/getlocation.js
@@ -1,9 +1,46 @@
+var xmlHTTP;
 function showLocation() {
 	var textValue = document.getElementById("macinfoinput").value;
 	if(textValue == ""){
 		var selectobj = document.getElementById("macinfoselect");
 		textValue = selectobj.options[selectobj.selectedIndex].value;
 	}
-	result = "<p> 位置是：<br/>" + textValue + "</p>";
-	document.getElementById("output").innerHTML =  result;
+
+	xmlHTTP = createXMLHttpRequest();
+	var url = "http://localhost:8080/TomcatTest/HelloWorld?mac="+textValue;
+	xmlHTTP.open("GET",url,true);
+	xmlHTTP.onreadystatechange = doResult;
+	xmlHTTP.setRequestHeader("Content-type","application/x-www-form-urlencoded;");
+	xmlHTTP.send();
+	
 }
+
+function doResult(){
+	if (xmlHTTP.readyState == 4) {
+		if (xmlHTTP.status == 200) {
+			result = "<p> 位置是：<br/>" + xmlHTTP.responseText + "</p>";
+			document.getElementById("output").innerHTML =  result;
+		}
+	}
+}
+function createXMLHttpRequest(){
+	var xmlHTTP;
+	if (window.XMLHttpRequest) {
+		xmlHTTP = new XMLHttpRequest();
+		if (xmlHTTP.overrideMimeType) {
+			xmlHTTP.overrideMimeType("text/xml");
+		}
+	}else if(window.ActiveXObject){
+		try{
+			xmlHTTP = new ActiveXObject("Msxml2.XMLHTTP");
+		}catch(e){
+			try{
+				xmlHTTP = new ActiveXObject("Microsoft.XMLHTTP");
+			}catch(e){
+
+			}
+		}
+	}
+	return xmlHTTP;
+}
+
