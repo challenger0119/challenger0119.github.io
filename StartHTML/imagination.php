24d3f53a20ebe9111f9bb1514a2544d1a8d276dd
miaoqi <miaoqi0119@163.com>
2017-11-28 23:12:31 +0800

    添加显示屏幕适应

--- StartHTML/imagination.php
+++ StartHTML/imagination.php
@@ -1,5 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Your Imaginations</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
 <?php
-include "showimagination.php";
+include "imaginationclass.php";
+
+function showImaginations(){
+
+	$mysql = new ItemMysql();
+	$itemArray = $mysql->getAllItems();
+	for ($i=0; $i < count($itemArray); $i++) {
+		$item = $itemArray[$i]; 
+		echo "<p>";
+		echo $item->date."<br/>";
+		echo $item->content."<br/>";
+		echo $item->location."<br/>";
+		echo "</p>";
+	}
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
@@ -15,7 +37,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 	$mysql->insertItem($item);
 
 }
-
 showImaginations();
-
 ?>
+</body>
+</html>
+
+
--- publish.sh
+++ publish.sh
@@ -2,4 +2,3 @@
 cp -f StartHTML/imagination.php /var/www/StartHTML
 cp -f StartHTML/imaginationclass.php /var/www/StartHTML
 cp -f StartHTML/imaginationweb.html /var/www/StartHTML
-cp -f StartHTML/showimagination.php /var/www/StartHTML
