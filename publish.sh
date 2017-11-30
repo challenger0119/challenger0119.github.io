3e01af1fa2283ca34b9663f7db6642deca83018c
Ubuntu <miaoqi0119@163.com>
2017-11-30 23:42:06 +0800

    modifed publish: add getlocation.js

--- publish.sh
+++ publish.sh
@@ -2,3 +2,4 @@
 cp -f StartHTML/imagination.php /var/www/StartHTML
 cp -f StartHTML/imaginationclass.php /var/www/StartHTML
 cp -f StartHTML/imaginationweb.html /var/www/StartHTML
+cp -f StartHTML/getlocation.js /var/www/StartHTML
